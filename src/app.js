3adc6bd92a43266b33578df43ebc1944e5307f4f
dianalves3 <dianalves3@gmail.com>
2019-11-02 14:53:00 +0000

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -44,8 +44,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "8e7395d4f989412fff4eb060663c2eeb";
-let city = "Porto";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "8e7395d4f989412fff4eb060663c2eeb";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Porto");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
